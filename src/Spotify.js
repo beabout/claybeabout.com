0d27f0788792d5981c567ab322364c147438d87c
Clay Beabout <beaboutclayton@gmail.com>
2023-03-14 21:26:12 -0500

    spotify: show only Clay's albums

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./index.scss";
-import { Grid, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import FadeIn from 'react-fade-in';
 import $ from "jquery";
+import claySpotifyAlbumsJSON from './Spotify.json';
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 const redirectUri = "https://claybeabout.com/discography";
 const sp_api_url = "https://api.spotify.com/v1"
 const get_user_albums = `${sp_api_url}/me/albums?limit=50&`;
-const get_user_playlists = `${sp_api_url}/users/cbeeb121/playlists?limit=20&`;
 
 const scopes = [
   "user-library-read",
@@ -37,18 +37,24 @@ class Spotify extends React.Component {
     super(props)
     this.state = { 
       token: null,
+      clayAlbums: claySpotifyAlbumsJSON.albums,
       albums: [],
-      playlists: []
     }
     this.getUserAlbums = this.getUserAlbums.bind(this);
   }
 
   updateAlbums = (res) => {
-    console.log(res.items);
     res.items.forEach(elem => {
       this.state.albums.push({
         art: elem.album.images[0].url,
-        artists: elem.album.artists,
+        artists: [
+          elem.album.artists.map(artist => { 
+            return { 
+              name: artist.name, 
+              href: artist.href 
+            }
+          })
+        ],
         name: elem.album.name,
         url: elem.album.external_urls.spotify
       })
@@ -56,26 +62,6 @@ class Spotify extends React.Component {
     this.setState({
       albums: this.state.albums
     });
-  }
-
-  updatePlaylists = (res) => {
-    res.items.forEach(elem => {
-      this.state.playlists.push({
-        art: (elem.images.length > 0 ? elem.images[0].url : ''),
-        desc: elem.description,
-        id: elem.id,
-        name: elem.name,
-        url: elem.external_urls.spotify,
-        tracks: elem.tracks
-      })
-    });
-    this.setState({
-      playlists: this.state.playlists
-    });
-  }
-  
-  updateSpotifyFile = (data) => {
-    var albums_string = JSON.stringify(this.state.albums)
   }
 
   getUserAlbums = (token, offset) => {
@@ -94,23 +80,6 @@ class Spotify extends React.Component {
     });
   }
 
-  getUserPlaylists = (token, offset) => {
-    $.ajax({
-      url: get_user_playlists + 'offset=' + offset,
-      type: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      success: function (res) {
-        if (res.items.length > 0) {
-          this.updatePlaylists(res);
-          this.getUserPlaylists(token, offset + 20)
-          console.log(res)
-        }
-      }.bind(this)
-    });
-  }
-
   componentDidMount() {
     let _token = hash.access_token;
     if (_token) {
@@ -118,43 +87,24 @@ class Spotify extends React.Component {
         token: _token
       });
       this.getUserAlbums(_token, 0)
-      this.getUserPlaylists(_token, 0)
     }
   }
 
   render() {
     return (
       <FadeIn>
-        {!this.state.token && (
-          <div>
-            <h2 style={{ marginTop: '3rem' }}>discography.</h2>
-            <p>
-              login to see your spotify record collection
-            </p>
-            <p>
-              <a 
-                className="btn-primary"
-                href={`${authEndpoint}?client_id=${getClientID()}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-              >
-                login
-              </a>
-            </p>
-          </div>
-        )}
-        {this.state.albums.length > 0 && (
-          <div>
-            <h2 style={{ marginTop: '3rem' }}>your collection.</h2>
-            <Grid className='p-100' container spacing={2}>
-              { this.state.albums.map(album => (
-                <Grid className='album' item sm={6} md={4}>
-                  <a href={album.url} target="_blank">
-                    <img src={album.art} style={{width:'100%'}} />
-                  </a>
-                </Grid>
-              ))}
-            </Grid>
-          </div>
-        )}
+        <div>
+          <h2 style={{ marginTop: '3rem' }}>my collection.</h2>
+          <Grid className='p-5' container spacing={1}>
+            {this.state.clayAlbums.map(album => (
+              <Grid className='album' item sm={2} md={2}>
+                <a href={album.url} target="_blank">
+                  <img src={album.art} className='album-art' />
+                </a>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </FadeIn>
     );
   }
