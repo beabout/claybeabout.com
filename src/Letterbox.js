c383e45839da1cb12c5e4f99fb6bbac278bf9432
Clay Beabout <beaboutclayton@gmail.com>
2023-07-09 21:47:20 -0500

    letterbox: add review ordering

--- src/Letterbox.js
+++ src/Letterbox.js
@@ -4,17 +4,28 @@ import FadeIn from 'react-fade-in';
 import claysLetterboxReviews from "./reviews.json"
 import { Grid } from '@mui/material';
 import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
-import $ from 'jquery';
 
-function compareDate(a, b) {
-  return a["Date"] < b["Date"];
+function compareWatchedDate(a, b) {
+  return a["Watched Date"] < b["Watched Date"];
+}
+
+function compareName(a, b) {
+  return a["Name"] > b["Name"];
+}
+
+function compareYear(a, b) {
+  return a["Year"] < b["Year"];
+}
+
+function compareRating(a, b) {
+  return a["Rating"] < b["Rating"];
 }
 
 class Letterbox extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      reviews: claysLetterboxReviews.reviews.sort(compareDate),
+      reviews: claysLetterboxReviews.reviews.sort(compareWatchedDate),
     }
   }
 
@@ -43,6 +54,12 @@ class Letterbox extends React.Component {
     return html;
   }
 
+  handleChange(e) {
+    console.log(e.target.value);
+    this.state = {
+      reviews: claysLetterboxReviews.reviews.sort(compareWatchedDate)
+    }
+  }
 
   render() {
     return(
@@ -52,6 +69,30 @@ class Letterbox extends React.Component {
           reviews rank from <FaStarHalfAlt /> to <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
         </hint>
         <Grid className='p-5' container spacing={1}>
+          <p style={{ padding: '1rem' }}>
+            Sort by
+            <select defaultValue="watched_date" onChange={(e) => {
+              console.log(e.target.value);
+              if(e.target.value == "Name") {
+                console.log("in Name");
+                this.setState({ reviews: claysLetterboxReviews.reviews.sort(compareName) });
+              } else if (e.target.value == "Year") {
+                console.log("in Year");
+                this.setState({ reviews: claysLetterboxReviews.reviews.sort(compareYear) });
+              } else if (e.target.value == "Rating") {
+                console.log("in Rating");
+                this.setState({ reviews: claysLetterboxReviews.reviews.sort(compareRating) });
+              } else {
+                console.log("in Date");
+                this.setState({ reviews: claysLetterboxReviews.reviews.sort(compareWatchedDate) });
+              }
+            }}>
+              <option value="Date">Watched Date</option>
+              <option value="Name">Name</option>
+              <option value="Year">Year</option>
+              <option value="Rating">Rating</option>
+            </select>
+          </p>
           {this.state.reviews.map(review => (
             <Grid item sm={12} md={12}>
               <div className='review'>
